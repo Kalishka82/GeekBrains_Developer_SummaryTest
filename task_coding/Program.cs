e29f5fd6746461cb488f60eae2aba2ef6035e2cb
Tatiana Kalinina <tankakalinka@gmail.com>
2022-10-10 17:41:05 +0300

    add initial code

---
+++ task_coding/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+Console.Clear();
+
+string[] stringArray = {"hello", "2", "world", "!", "-65", "Kazan", "and", "computer science", ":)"};
+
+PrintNewArray(stringArray);
+
+void PrintNewArray(string[] array)
+{
+    string[] newArray = Array.FindAll(array, item => item.Length <=3);
+
+    for (int idx = 0; idx < newArray.Length; idx++)
+    {
+        if (idx != newArray.Length - 1)
+            Console.Write($"{newArray[idx]}, ");
+        else
+            Console.WriteLine($"{newArray[idx]}");
+    }
+}
+
+
+// Вариант 2:
+// for (int idx = 0; idx < stringArray.Length; idx++)
+// {
+//     if (stringArray[idx].Length <= 3)
+//     {
+//         Console.Write($"{stringArray[idx]}, ");
+//     }
+// }
